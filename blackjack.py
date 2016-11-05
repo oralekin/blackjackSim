97fb96ff27527a71673d4ac3035b1b3909d20289
Ekin <oralekin@gmail.com>
2016-11-05 11:39:19 +0300

    added randomizer

--- blackjack.py
+++ blackjack.py
@@ -1,15 +1,41 @@
 import random
 
-deste = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10", "S11", "S12", "S13",
-         "K01", "K02", "K03", "K04", "K05", "K06", "K07", "K08", "K09", "K10", "K11", "K12", "K13",
-         "Q01", "Q02", "Q03", "Q04", "Q05", "Q06", "Q07", "Q08", "Q09", "Q10", "Q11", "Q12", "Q13",
-         "M01", "M02", "M03", "M04", "M05", "M06", "M07", "M08", "M09", "M10", "M11", "M12", "M13"]
+jokersizdeste = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10", "S11", "S12", "S13",
+                 "K01", "K02", "K03", "K04", "K05", "K06", "K07", "K08", "K09", "K10", "K11", "K12", "K13",
+                 "Q01", "Q02", "Q03", "Q04", "Q05", "Q06", "Q07", "Q08", "Q09", "Q10", "Q11", "Q12", "Q13",
+                 "M01", "M02", "M03", "M04", "M05", "M06", "M07", "M08", "M09", "M10", "M11", "M12", "M13"]
 # Sinek Karo Kupa Maça
 
 distaki = []
+deste = []
+
+class PlayerDefs:
+    def __init__(self):
+        self.el = []
+        self.score = 0
+        self.toplam = 0
+
+    def toplamhesabı(self):
+        self.toplam = 0
+        for kart in self.el:
+            self.toplam += Kart.deger(kart)
+        return self.toplam
+
+
 class Kart:
     @staticmethod
-    def kartcek(self):
+    def kartcek(playernaem, deste):
+        playernaem.el.append(deste[-1])
+        del deste[-1]
+
+    @staticmethod
+    def deger(kart):
+        return int(kart[1] + kart[2])
+
+player = PlayerDefs()
+computer = PlayerDefs()
+print(player)
+
 
 def karistir(listt, ite):
     for _ in range(1, ite):
@@ -21,4 +47,15 @@ def karistir(listt, ite):
             i += 1
     return listt + distaki
 
-deste = karistir(deste, 7)
+
+def runprog(destestart):
+    deste = karistir(destestart, 7)
+    Kart.kartcek(player, deste)
+    Kart.kartcek(computer, deste)
+    Kart.kartcek(player, deste)
+    Kart.kartcek(computer, deste)
+    print(player.el)
+
+
+runprog(jokersizdeste)
+print(123)
